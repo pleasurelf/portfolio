dfa397b5834707fc80470e686c741cfd6fc6184d
pleasurelf <jennekilroy@gmail.com>
2026-04-17 20:48:32 +0930

    Removing more projects unrelated to my work

--- lib/projects/index.ts
+++ lib/projects/index.ts
@@ -5,39 +5,23 @@ export type { Project, ProjectSection, ProjectLink } from "./types";
 // Edit this list to add, remove, or reorder projects.
 // Each import is the project's own file in this folder.
 
-import { testbuddy } from "./testbuddy";
-import { legoParty } from "./lego-party";
 import { fitbitArcade } from "./fitbit-arcade";
 import { guiltyGear } from "./guilty-gear";
 import { dreamlight } from "./dreamlight";
-import { innchanted } from "./innchanted";
-import { forestDemo } from "./forest-demo";
-import { factoryDemo } from "./factory-demo";
 import { stel } from "./stel";
 import { doomsdayBall } from "./doomsday-ball";
 import { herbiesHeinousHorrors } from "./herbies-heinous-horrors";
 import { findersKeepers } from "./game-jam-finders-keepers";
-import { goap } from "./goap";
-import { playerController } from "./player-controller";
-import { procgen } from "./procgen";
 
 export const projects = [
   // Professional
-  testbuddy,
-  legoParty,
   dreamlight,
   guiltyGear,
   fitbitArcade,
-  innchanted,
   // Personal
     stel,
     doomsdayBall,
     herbiesHeinousHorrors,
-  forestDemo,
-  factoryDemo,
-  procgen,
-  goap,
-  playerController,
   // Game Jams
     findersKeepers,
 ];
