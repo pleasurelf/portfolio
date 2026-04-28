651d3f264b3a5686be02eceedde7bda5beacd348
pleasurelf <jennekilroy@gmail.com>
2026-04-29 08:44:27 +0930

    Formatting fixing, adding doomsday ball files

--- lib/projects/project-doomsday-ball.ts
+++ lib/projects/project-doomsday-ball.ts
@@ -1,6 +1,7 @@
 ﻿import type { Project } from "./types";
 
-export const doomsdayBall: Project = {
+export const doomsdayBall: Project = 
+{
     id: "project-doomsday-ball",
     title: "Doomsday Ball",
     year: "2020",
@@ -9,7 +10,7 @@ export const doomsdayBall: Project = {
     category: "personal",
     image: "/doomsday-ball/doomsday-ball-cover.png",
     description:
-        "A silly mystery narrative about Frankie, a sweet lizard boy who slithered his way to prom. Have a blast from the past in the barren Moore Jupiter High gymnasium, interacting with the remnants left behind to discover where the the prom queen went, and why you've arrived just a little too late to the ball.",
+        "A silly mystery narrative about Frankie, a sweet lizard boy who slithered his way to prom.",
     tags: ["Unity", "C#", "Procreate", "Notion"],
     links: [
         {
@@ -18,39 +19,37 @@ export const doomsdayBall: Project = {
             type: "external",
         },
     ],
+    
     fullDescription: `
-Stel is a short mystery-narrative about a boy instructed to seek help from a witch doctor in order to save his ailing papa. He is diverted on his path when the witch informs him that she requires a favour in return, and asks the boy to retrieve a mysterious item. As the story plays out, the urgency of helping his papa might be blinding Stel from the true motive behind his journey.
-  `,
+        Stel is a short mystery-narrative about a boy instructed to seek help from a witch doctor in order to save his ailing papa. He is diverted on his path when the witch informs him that she requires a favour in return, and asks the boy to retrieve a mysterious item. As the story plays out, the urgency of helping his papa might be blinding Stel from the true motive behind his journey.
+    `,
+    
     sections: [
-        // TODO get gameplay footage?
-//         {
-//             title: "ACME Introduction",
-//             body: `
-// something about being nominated
-//       `,
-//             videoBelow: "/videos/stel/stel-introduction.mp4",
-//         },
         {
-            title: "Protodype",
-            body: `
-DOOMSDAY BALL IS A PROTOTYPE GAME I WORKED ON AS A GAME DESIGNER, PROGRAMMER, PRODUCER, AND WRITER. I WORKED ON THIS GAME OVER FOUR WEEKS, AND WHILE LARGELY UNFINISHED, IT REMAINS ONE OF MY FAVOURITE CONCEPTS.
-      `,
+            title: "Prototype",
             image: "",
             imageAlt: "dfgdfgdfg",
+            body: `
+                DOOMSDAY BALL IS A PROTOTYPE GAME I WORKED ON AS A GAME DESIGNER, PROGRAMMER, PRODUCER, AND WRITER. I WORKED ON THIS GAME OVER FOUR WEEKS, AND WHILE LARGELY UNFINISHED, IT REMAINS ONE OF MY FAVOURITE CONCEPTS.
+            `,
         },
+        
         {
             title: "dfgdfg",
-            body: `
-            It was supposed to be simple trip to the doctor, but instead of medicine you received a map, and were shoved into an unfamiliar land to have your mettle tested.
-            `,
             image: "",
             imageAlt: "gfhdfghd",
+            body: `
+                It was supposed to be simple trip to the doctor, but instead of medicine you received a map, and were shoved into an unfamiliar land to have your mettle tested.
+            `,
         },
+        
         {
             title: "dfgsdfg",
-            body: `dfgdfgdfg`,
             image: "",
             imageAlt: "dfgdfg",
+            body: `
+                dfgdfgdfg
+            `,
         },
     ],
 };
